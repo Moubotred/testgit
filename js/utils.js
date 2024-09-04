133c6c35bba075146aabe27630ace47c45a84a36
Moubotred <fakesarnamer@gmail.com>
2024-09-03 22:30:16 -0500

    corregion de errores| uso de primera ves de promesas de js funcion execution_cmd

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,6 @@
+const {exec} = require('child_process');
 const fs = require('fs');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 
 function help(message) {
     const banner = 'Bienvenido al bot:\n\ncomandos:\n\n/lg suscribir al bot\n/s obtener url\n/d obtener pdf';
@@ -6,8 +8,9 @@ function help(message) {
 }
 
 // Función para guardar suscriptores en un archivo JSON
-function guardarSuscriptores(filesuscription, suscriptores) {
+function guardarSuscriptores(filesuscription, suscriptores, message) {
     fs.writeFileSync(filesuscription, JSON.stringify(suscriptores, null, 2));
+    message.reply('Usuario registrado');
 }
 
 // Función para cargar suscriptores desde un archivo JSON
@@ -19,14 +22,61 @@ function database(filesuscription) {
     return suscriptores;
 }
 
-function Argument_Management(partes){
+function argument_management(partes){
     message.reply(`Respuesta: command ${partes[0]} requiere suministro`);
     console.error(`ReponsePython: command ${partes[0]} requiere suministro`);
     return;
 }
 
+function sendfile(evalue,numero,message){
+
+
+    if (evalue.trim() === 'suministro no existe en base de datos') {
+        message.reply(`Suministro No Existe`);
+
+    } else if (evalue.trim().endsWith('.pdf')) {
+        const pdf = MessageMedia.fromFilePath(`${__dirname}/../py/pdf/${numero}.pdf`);
+        message.reply(`Respuesta: ${evalue}`, undefined, { media: pdf, quotedMessageId: message.id._serialized });
+        console.log(`ReponsePython: envio existoso ${evalue}`);
+    }
+
+        // else{
+        //     message.reply(`Respuesta: ${stdout.trim()}`);
+        // };
+    }
+
+
+    // console.log(`ReponsePython: ${stdout}`);
+// }
+
+function execution_cmd(suministro, mode, message) {
+    // Validación básica de parámetros
+    if (typeof suministro !== 'string' || typeof mode !== 'string') {
+        throw new Error('Los parámetros suministro y mode deben ser cadenas');
+    }
+
+    return new Promise((resolve, reject) => {
+        exec(`python3 /home/kimshizi/Documents/test/py/Utils.py ${suministro} --mode ${mode}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error ejecutando el script: ${error.message}`);
+                reject(error);
+                return;
+            }
+            if (stderr) {
+                console.error(`Error estándar: ${stderr}`);
+                reject(stderr);
+                return;
+            }
+            resolve(stdout);
+        });
+    });
+}
+
 module.exports = {
     help,
     guardarSuscriptores,
-    database
+    database,
+    argument_management,
+    execution_cmd,
+    sendfile
 };
