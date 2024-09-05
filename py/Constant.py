9171c9c807dafe2fd5c9e20432984f1351109e2e
Moubotred <fakesarnamer@gmail.com>
2024-09-04 23:27:20 -0500

    se agrego nueva constante llamada cargo

--- py/Constant.py
+++ py/Constant.py
@@ -5,4 +5,5 @@ class exp:
     table = '//table[@class="datagrid-btable"]/tbody/tr'
     info = '/html/body/div[1]/div[2]/div/div[1]/div/div/div[1]/div/div/div[2]/div[1]/div/div/div[1]/div/table[1]/tbody/tr[2]/td/span/br'
     tr = '/html/body/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/table/tbody/tr[@]/td[1]/div'
+    cargo = 'btndetcargo'
     # tr = '//td[@field="estado_solo"]'
