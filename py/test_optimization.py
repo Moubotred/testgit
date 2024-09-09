c2a31f65739236a50e7717fb8fb0a4217cb8841b
Moubotred <fakesarnamer@gmail.com>
2024-09-09 00:03:21 -0500

    se agrego nuevas rutas del endpoint para convertir al url a pdf y imagena pdf

--- py/test_optimization.py
+++ py/test_optimization.py
@@ -12,16 +12,20 @@ import time
 from flask import Flask, request, jsonify
 import ResourceHub as Rb
 import requests
+import threading
+from ResourceHub import Servicios
+import Constant as C 
+
+import json
 
 app = Flask(__name__)
 
-# Variables globales para el WebDriver y WebDriverWait
+# Global variables for WebDriver and WebDriverWait
 driver = None
 wait = None
 
-
-
-def medir_tiempo(func):
+# decoraror para medir tiempo
+def measure_time(func):
     """
     Decorador para medir el tiempo de ejecución de una función.
     
@@ -32,94 +36,114 @@ def medir_tiempo(func):
         wrapper: Función decorada que mide el tiempo de ejecución.
     """
     def wrapper(*args, **kwargs):
-        inicio = time.time()
+        start = time.time()
         try:
-            resultado = func(*args, **kwargs)
+            result = func(*args, **kwargs)
         except Exception as e:
-            print(f"Error en {func.__name__}: {e}")
+            print(f"Error in {func.__name__}: {e}")
             raise
-        fin = time.time()
-        print(f"{func.__name__} tomó {fin - inicio:.2f} segundos")
-        return resultado
+        end = time.time()
+        print(f"{func.__name__} took {end - start:.2f} seconds")
+        return result
     return wrapper
 
-def cargo_de_envios(wait):
+# busca el frame que contiene la url
+def shipping_charge(wait):
+    """
+    Determina el tipo de documento de carga basado en la información contenida
+    en una tabla y devuelve la URL del frame del subdocumento asociado.
+
+    Args:
+        wait (WebDriverWait): Objeto de espera explícita para sincronizar
+        el código con la carga de elementos en la página web.
+
+    Returns:
+        str: URL del frame del subdocumento si el documento es una carta 
+        de reemplazo de medidor de empresas. Si el documento es un aviso 
+        de corte, devuelve un mensaje específico. Si el documento es 
+        postales, devuelve otro mensaje específico.
+
+    Raises:
+        No se especifican excepciones."""
+    
     time.sleep(3)
     table_send = wait.until(EC.presence_of_all_elements_located((By.XPATH, C.exp.table)))
     for n_ in range(len(table_send)):
         table_tr = wait.until(EC.presence_of_element_located((By.XPATH,C.exp.tr.replace('@',f'{n_+1}')))).click()
-        # time.sleep(2)
         info = wait.until(EC.presence_of_element_located((By.ID,'sp_cargo_documento'))).text
         if 'CARTAS / REEMPLAZO DE MEDIDOR EMPRESAS' == info:
             wait.until(EC.presence_of_element_located((By.ID, C.exp.cargo))).click()
-            # //*[@id="btndetcargo"]
-            # time.sleep(1)
             Framesubdoc = wait.until(EC.presence_of_element_located((By.ID, C.exp.subdoc))).get_attribute('src')
             return Framesubdoc
         elif 'VOLANTES / AVISO DE CORTE' == info:
-            return 'No hay carta de CAMBIO DE MEDIDOR solo AVISO DE CORTE'
+            return 'No METER CHANGE letter, only CUT NOTICE'
         elif 'RECIBO / POSTALES' == info:
-            return 'No hay carta de CAMBIO DE MEDIDOR solo POSTALES'
-    
-@medir_tiempo
-def test_add(wait, suministro):
+            return 'No METER CHANGE letter, only POSTCARDS'
 
+# carga el numero de suministro y lo envia    
+@measure_time
+def send_supply(wait, supply):
     """
     Interactúa con una página web para enviar un suministro y obtener un atributo del iframe.
     
     Args:
         wait: Objeto WebDriverWait para manejar las esperas explícitas.
-        suministro: El valor a enviar en el formulario.
+        supply: El valor a enviar en el formulario.
     
     Returns:
         str: El valor del atributo 'src' del iframe.
     """
-
     try:
         time.sleep(2)
         sandbox = wait.until(EC.presence_of_element_located((By.ID, C.exp.box)))
-        sandbox.send_keys(suministro)
+        sandbox.send_keys(supply)
 
         sendContent = wait.until(EC.presence_of_all_elements_located((By.XPATH, C.exp.btn)))
         sendContent[0].click()
 
-        
-
-        rs = cargo_de_envios(wait)
+        rs = shipping_charge(wait)
         return rs
-    
-        # time.sleep(3)
-        # Framesubdoc = wait.until(EC.presence_of_element_located((By.ID, C.exp.subdoc))).get_attribute('src')
-        # return Framesubdoc
 
     except Exception as e:
-        print(f"Error en test_add: {e}")
+        print(f"Error in send_supply: {e}")
         return None
 
-def test_queue(driver, wait, suministro):
+# convierte documento a un pdf
+@measure_time
+def pdf_converter(response, supply):
+    result = response.json()
+    result_url = result.get('result')
+    url_ = Rb.UrlSubdoc(result_url)
+    filename = Rb.FileWebDownloads(url_, supply)
+    ff = Rb.ConvertPdf(filename)
+    Rb.Templades(ff)
+    return ff
+
+# maneja las peticiones en cola
+def Supply_tail(driver, wait, supply):
     """
-    Maneja la cola de suministros, abre nuevas pestañas, y llama a test_add para un suministro.
+    Maneja la cola de suministros, abre nuevas pestañas, y llama a send_supply para un suministro.
     
     Args:
         driver: Instancia del WebDriver.
         wait: Objeto WebDriverWait para manejar las esperas explícitas.
-        suministro: Valor del suministro para procesar.
+        supply: Valor del suministro para procesar.
     
     Returns:
         str: Resultado del procesamiento del suministro.
     """
-    cola = Queue()
-    cola.put(suministro)
+    queue = Queue()
+    queue.put(supply)
     result = None
 
-    while not cola.empty():
-        elemento = cola.get()
+    while not queue.empty():
+        element = queue.get()
         try:
             driver.execute_script("window.open('https://hasbercourier.easyenvios.com/', '');")
             driver.switch_to.window(driver.window_handles[-1])
-            result = test_add(wait, elemento)
+            result = send_supply(wait, element)
         except Exception as e:
-            print(f"Error al procesar elemento {elemento}: {e}")
+            print(f"Error processing element {element}: {e}")
             result = None
         finally:
             driver.close()
@@ -127,6 +151,7 @@ def test_queue(driver, wait, suministro):
     
     return result
 
+# inicia el navegador
 def init_browser():
     """
     Inicializa el WebDriver y abre la página principal al iniciar la aplicación.
@@ -137,14 +162,16 @@ def init_browser():
     driver = webdriver.Firefox(options=options)
     wait = WebDriverWait(driver, 60)
     try:
-        driver.get('https://hasbercourier.easyenvios.com/')        
+        driver.get('https://hasbercourier.easyenvios.com/')
+        threading.Thread(target=Servicios, daemon=True).start()
     except Exception as e:
-        print(f"Error al abrir la página principal: {e}")
+        print(f"Error opening main page: {e}")
         driver.quit()
         raise
 
-@app.route('/procesar_suministro', methods=['POST'])
-def procesar_suministro():
+# endpoint para obtener el frame y ver la url
+@app.route('/process_supply', methods=['POST'])
+def process_supply():
     """
     Endpoint para procesar un suministro.
     
@@ -155,39 +182,49 @@ def procesar_suministro():
         JSON response with the result of processing the suministro.
     """
     data = request.json
-    suministro = data.get('suministro')
+    supply = data.get('suministro')
     
-    if suministro is None:
-        return jsonify({"error": "Debe proporcionar un suministro."}), 400
+    if supply is None:
+        return jsonify({"error": "You must provide a supply."}), 400
     try:
-        result = test_queue(driver, wait, suministro)
+        result = Supply_tail(driver, wait, supply)
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
     return jsonify({"result": result})
 
-@app.route('/procesar_convertir_pdf', methods=['POST'])
-def procesar_suministro():
+# endpoint para convertir el frame a un pdf
+@app.route('/process_convert_pdf', methods=['POST'])
+def process_convert_pdf():
     data = request.json
-    suministro = data.get('suministro')
-    if suministro is None:
-        return jsonify({"error": "Debe proporcionar un suministro."}), 400
+    supply = data.get('suministro')
 
-    # Llamar al endpoint que procesa el suministro y obtiene la URL del PDF
-    response = requests.post("http://localhost:5000/procesar_suministro", json={"suministro": suministro})
+    if supply is None:
+        return jsonify({"error": "You must provide a supply."}), 400
+
+    # # Call the endpoint that processes the supply and gets the PDF URL
+    response = requests.post("http://localhost:5000/process_supply", json={"suministro": supply})
     if response.status_code != 200:
-        return jsonify({"error": "Error al procesar el suministro."}), response.status_code
+        return jsonify({"error": "Error processing the supply."}), response.status_code
 
-    result = response.json()
-    result_url = result.get('result')
-    url_ = Rb.UrlSubdoc(result_url)
-    filename = Rb.FileWebDownloads(url_)
-    ff = Rb.ConvertPdf(filename)
-    Rb.Templades(ff)
-
+    filename = pdf_converter(response, supply)
+    
     return jsonify({"result": filename})
 
+@app.route('/process_image_a_pdf', methods=['POST'])
+def process_imagen_pdf():
+    data = request.json
+    supply = data.get('suministro')
+    if supply is None:
+        return jsonify({"error": "You must provide a supply."}), 400
+
+    with open("/home/kimshizi/Documents/test/py/tmp/tunnel.json", "r") as archivo:
+        rs = json.load(archivo)
+        tunnel = rs.get('tunnel')
+    suministro = Rb.GoogleLents(driver,wait,tunnel,'sum.jpg')
+    return jsonify({"result": suministro})    
+
 if __name__ == "__main__":
-    init_browser()  # Inicializa el navegador cuando se inicia la aplicación
-    app.run(host='0.0.0.0',port=5000)
+    init_browser()  # Initialize the browser when the application starts
+    app.run(host='0.0.0.0', port=5000)
