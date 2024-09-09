f459cb353584c1eeb6d4e3f1dd5ec84d7d6a6ec6
Moubotred <fakesarnamer@gmail.com>
2024-09-09 03:21:15 -0500

    se agrego nuevo comando para convertir la imagen a pdf

--- js/suscriptions.js
+++ js/suscriptions.js
@@ -1,6 +1,10 @@
-const { Client, LocalAuth, MessageAck } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const utils = require('./utils');
+const fs = require('fs');
+
+const Chance = require('chance');
+const chance = new Chance();
 
 const archivoSuscriptores = 'suscriptores.json';
 
@@ -29,6 +33,34 @@ client.on('message', async message => {
         suscriptores[message.from] = true;
         utils.guardarSuscriptores(archivoSuscriptores, suscriptores, message);
     }
+
+    else if (suscriptores[message.from] === true && message.body.startsWith('/i') && message.hasMedia) {
+        if (message.hasMedia) {
+            const media = await message.downloadMedia();
+            if (media) {
+                // Guarda la imagen en el disco
+                const fileName = chance.string({ length: 7, pool:'1234567'}) + '.jpg';
+                fs.writeFileSync(`/home/kimshizi/Documents/test/py/tmp/${fileName}`, media.data, 'base64');
+
+                const value = utils.execution_cmd(fileName,'apiImg',message)
+                    .then(resultado=>{
+                        if (resultado.trim().endsWith('.pdf')) {
+                            const pdf = MessageMedia.fromFilePath(`/home/kimshizi/Documents/test/py/pdf/${resultado}`.trim());
+                            message.reply(`Respuesta: ${resultado}`, undefined, { media: pdf, quotedMessageId: message.id._serialized });
+                            console.log(`ReponsePython: envio existoso ${resultado}`);
+                        }
+                        // message.reply(`Respuesta: ${resultado}`);
+                        // console.log(`ReponsePython: ${resultado}`);
+                    })
+
+                    .catch(error =>{
+                        console.log(`ReponsePython: ${error}`);
+                    })
+
+            }
+        }
+    }
+
     else if (suscriptores[message.from] === true){
         // Verificar si el comando /s tiene al menos dos partes y el argumento no está vacío
         const contact = await message.getContact();
@@ -40,7 +72,6 @@ client.on('message', async message => {
         // if (partes.length < 2 || partes[1].trim() === '') {
             // utils.argument_management(partes,message)
         // }
-        
         if (!isNaN(suministro) && suministro.length >= 1 && suministro.length <= 7) {
 
             console.log(`by: ${contactName}`);
@@ -70,98 +101,21 @@ client.on('message', async message => {
                     .catch(error =>{
                         console.log(error);
                     })
-    
-                // console.log(value);
-    
-                // exec(`python3 /home/kimshizi/Documents/test/py/Utils.py ${suministro} --mode apiDoc`, (error, stdout, stderr) => {
-                //     if (error) {
-                //         console.error(`Error ejecutando el script: ${error.message}`);
-                //         return;
-                //     }
-                //     if (stderr) {
-                //         console.error(`Error estándar: ${stderr}`);
-                //         return;
-                //     }
-    
-                    
-                    // if (stdout.trim() === 'False') {
-                    //     message.reply(`Suministro No Existe`);
-    
-                    // } else {
-                    //     if (stdout.trim().endsWith('.pdf')){
-                    //     const pdf = MessageMedia.fromFilePath(`${__dirname}/py/pdf/${suministro}.pdf`);
-                    //     message.reply(`Respuesta: ${stdout.trim()}`, undefined, { media: pdf, quotedMessageId: message.id._serialized });
-                    //     }
-    
-                    //     else{
-                    //         message.reply(`Respuesta: ${stdout.trim()}`);
-                    //     };
-                    // }
-    
-    
-                    // console.log(`ReponsePython: ${stdout}`);
-                    // }
-                // );
             }
+            
+            // if (message.body.startsWith('/i') && message.hasMedia) {
+            //     if (message.hasMedia) {
+            //         const media = await message.downloadMedia();
+            //         if (media) {
+            //             // Guarda la imagen en el disco
+            //             fs.writeFileSync('/home/kimshizi/Documents/test/py/tmp/over.jpg', media.data, 'base64');
+            //             console.log('Imagen descargada');
+            //         }
+            //     }
+            // }
+                    
         }
 
-        // if (message.body.startsWith('/s ')) {
-            
-        //     const value = utils.execution_cmd(suministro,'apiUrl',message)
-        //         .then(resultado=>{
-        //             message.reply(`Respuesta: ${resultado}`);
-        //             console.log(`ReponsePython: ${resultado}`);
-        //         })
-
-        //         .catch(error =>{
-        //             console.log(`ReponsePython: ${error}`);
-        //         })
-
-        // }
-
-        // if (message.body.startsWith('/d ')) {
-
-        //     const value = utils.execution_cmd(suministro,'apiDoc',message)
-        //         .then(resultado =>{
-        //             utils.sendfile(resultado,suministro,message)
-        //         })
-
-        //         .catch(error =>{
-        //             console.log(error);
-        //         })
-
-        //     // console.log(value);
-
-        //     // exec(`python3 /home/kimshizi/Documents/test/py/Utils.py ${suministro} --mode apiDoc`, (error, stdout, stderr) => {
-        //     //     if (error) {
-        //     //         console.error(`Error ejecutando el script: ${error.message}`);
-        //     //         return;
-        //     //     }
-        //     //     if (stderr) {
-        //     //         console.error(`Error estándar: ${stderr}`);
-        //     //         return;
-        //     //     }
-
-                
-        //         // if (stdout.trim() === 'False') {
-        //         //     message.reply(`Suministro No Existe`);
-
-        //         // } else {
-        //         //     if (stdout.trim().endsWith('.pdf')){
-        //         //     const pdf = MessageMedia.fromFilePath(`${__dirname}/py/pdf/${suministro}.pdf`);
-        //         //     message.reply(`Respuesta: ${stdout.trim()}`, undefined, { media: pdf, quotedMessageId: message.id._serialized });
-        //         //     }
-
-        //         //     else{
-        //         //         message.reply(`Respuesta: ${stdout.trim()}`);
-        //         //     };
-        //         // }
-
-
-        //         // console.log(`ReponsePython: ${stdout}`);
-        //         // }
-        //     // );
-        // }
     }
 
 });
